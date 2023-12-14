bcd10cff14ff4208b296b23e40828f2dae153842
Philipp Scheff <philippscheff@me.com>
2023-12-14 16:31:25 +0100

    use CanFrame & macro

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,9 @@ pub fn codegen(dbc_name: &str, dbc_content: &[u8], out: impl Write, debug: bool)
     writeln!(w, r##"#[cfg(feature = "arb")]"##)?;
     writeln!(&mut w, "use arbitrary::{{Arbitrary, Unstructured}};")?;
     writeln!(&mut w)?;
+    writeln!(&mut w, "use crate::util::can::CanFrame;")?;
+    writeln!(&mut w, "use r#macro::FromMessageForCanFrame;")?;
+    writeln!(&mut w)?;
 
     render_dbc(&mut w, &dbc).context("could not generate Rust code")?;
 
